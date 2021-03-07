5855e01e20b1c845bc8fc8ed8cd0e128a7fa8d68
Rony Peterson Assuncao Monteiro <rony.silva@mundiale.com.br>
2021-03-07 12:52:15 -0300

    setting up redux store

---
+++ screens/shop/ProductsOverViewScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductOverviewScreen = props => {
+    return <FlatList />
+};
+
+
+export default ProductOverviewScreen;
