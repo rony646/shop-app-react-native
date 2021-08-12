faa3e6720fd8303ff54b272b08a739bc45632ad0
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-08-12 20:41:08 -0300

    chore: hadling errors

--- screens/shop/ProductsOverViewScreen.js
+++ screens/shop/ProductsOverViewScreen.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+   FlatList, 
+   Button, 
+   Platform, 
+   ActivityIndicator, 
+   View,
+   StyleSheet,
+   Text
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import * as productsActions from '../../store/actions/products';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = props => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
+
+  const loadProducts = useCallback(async () => {
+    setError(null)
+    setIsLoading(true);
+    try {
+        await dispatch(productsActions.fetchProducts()).then()
+    } catch(err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+}, [dispatch])
+
+  useEffect(() => {
+      loadProducts()
+  } , [dispatch, loadProducts])
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
@@ -18,6 +45,33 @@ const ProductsOverviewScreen = props => {
       productTitle: title
     });
   };
+
+  if(error) {
+      return(
+        <View style={styles.centered}>
+            <Text>An error ocurred!</Text>
+            <Button title="Try again" onPress={loadProducts} color={Colors.primary}/>
+        </View>
+      )
+  }
+
+  if(isLoading) {
+    return(
+      <View style={styles.centered}>
+        <ActivityIndicator size='large' color={Colors.primary} />
+      </View>
+    )
+  }
+
+  if(!isLoading && products.length === 0) {
+    return(
+      <View style={styles.centered}>
+          <Text>
+              No products found. Maybe start adding some!
+          </Text>
+      </View>
+    )
+  }
 
   return (
     <FlatList
@@ -79,5 +133,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default ProductsOverviewScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,40 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+import Product from '../../models/product';
+
+export const fetchProducts = () => {
+    return async dispatch => {
+     try {
+      const response = await fetch('https://rn-complete-guite-default-rtdb.firebaseio.com/products.json')
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+  
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(new Product(
+            key, 
+            'u1', 
+            resData[key].title, 
+            resData[key].imageUrl, 
+            resData[key].description,
+            resData[key].price
+            )
+          )
+      };
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts })
+     } catch( err ) {
+        throw err
+     }
+  }
+};
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
