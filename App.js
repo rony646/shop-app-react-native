9c4704ebf60aafbbf79889aa3bfa2f561781ad09
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-08-12 19:58:04 -0300

    adding redux thunk and seding data to firebase

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font'
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 
 import productsReducer from './store/reducers/products';
@@ -17,7 +18,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
 
--- store/actions/products.js
+++ store/actions/products.js
@@ -7,15 +7,36 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
-  };
+  return async dispatch => {
+    const response = await fetch('https://rn-complete-guite-default-rtdb.firebaseio.com/products.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    });
+
+    const resData = await response.json();
+
+    console.log(resData);
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
+  }
+  
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
