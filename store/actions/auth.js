ca9b0e6a43dc2aa3067c0a53562999253d57f170
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-08-26 15:49:00 -0300

    fix: ommiting google api key

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,8 +1,9 @@
 export const SIGNUP = 'SIGNUP';
+import { GOOGLE_API_KEY } from '../../keys'
 
 export const signup = (email, password) => {
     return async dispatch => {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDhFXOrlsiycAFQZJbZBM_P38brY2k8Aqw', {
+        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${GOOGLE_API_KEY}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
