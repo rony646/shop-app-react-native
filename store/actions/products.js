c65ce25f17f81d9573dfaed152bf24b7be239e73
rony646 <rony646@users.noreply.github.com>
2021-08-28 20:31:45 -0300

    using user token for the requests

--- store/actions/products.js
+++ store/actions/products.js
@@ -6,8 +6,8 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
-  return async dispatch => {
-    // any async code you want!
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
         'https://rn-complete-guite-default-rtdb.firebaseio.com/products.json'
@@ -24,7 +24,7 @@ export const fetchProducts = () => {
         loadedProducts.push(
           new Product(
             key,
-            'u1',
+            resData[key].ownerId,
             resData[key].title,
             resData[key].imageUrl,
             resData[key].description,
@@ -33,7 +33,7 @@ export const fetchProducts = () => {
         );
       }
 
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts, userProducts: loadedProducts.filter(prod => prod.ownerId === userId) });
     } catch (err) {
       // send to custom analytics server
       throw err;
@@ -42,9 +42,10 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token
     const response = await fetch(
-      `https://rn-complete-guite-default-rtdb.firebaseio.com/products/${productId}.json`,
+      `https://rn-complete-guite-default-rtdb.firebaseio.com/products/${productId}.json?auth=${token}`,
       {
         method: 'DELETE'
       }
@@ -58,10 +59,11 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return async dispatch => {
-    // any async code you want!
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
-      'https://rn-complete-guite-default-rtdb.firebaseio.com/products.json',
+      `https://rn-complete-guite-default-rtdb.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
@@ -71,7 +73,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           title,
           description,
           imageUrl,
-          price
+          price,
+          ownerId: userId
         })
       }
     );
@@ -94,7 +97,6 @@ export const createProduct = (title, description, imageUrl, price) => {
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch, getState) => {
     const token = getState().auth.token
-    console.log('TOKEN: ', token)
     const response = await fetch(
       `https://rn-complete-guite-default-rtdb.firebaseio.com/products/${id}.json?auth=${token}`,
       {
